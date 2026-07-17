342742d4f2fc1fd4725ab8680635d6c90cf1487b
Mark Jim Francisco <88428314+mjfrncsco@users.noreply.github.com>
2026-07-18 00:15:53 +0800

    Homepage

---
+++ kei-khristian/script.js
@@ -0,0 +1,39 @@
+const seal = document.getElementById("seal");
+
+seal.addEventListener("click", () => {
+
+    // Next step:
+    // Open the envelope
+
+});
+
+const subtitleText = "A Love Letter From";
+const nameText = "Khristian & Kei";
+
+const subtitle = document.getElementById("typing-subtitle");
+const name = document.getElementById("typing-name");
+
+let subtitleIndex = 0;
+let nameIndex = 0;
+
+function typeSubtitle() {
+    if (subtitleIndex < subtitleText.length) {
+        subtitle.textContent += subtitleText.charAt(subtitleIndex);
+        subtitleIndex++;
+        setTimeout(typeSubtitle, 70);
+    } else {
+        setTimeout(typeName, 300); // konting pause bago names
+    }
+}
+
+function typeName() {
+    if (nameIndex < nameText.length) {
+        name.textContent += nameText.charAt(nameIndex);
+        nameIndex++;
+        setTimeout(typeName, 120);
+    }
+}
+
+window.addEventListener("load", () => {
+    setTimeout(typeSubtitle, 800);
+});
